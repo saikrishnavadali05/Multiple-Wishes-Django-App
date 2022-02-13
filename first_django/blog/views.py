df37633258b418b8b450ac02aaef25963a499b06
saikrishnavadali05 <saikrishnavadali05@gmail.com>
2022-02-13 12:23:33 +0530

    remove unnecessary imports

--- first_django/blog/views.py
+++ first_django/blog/views.py
@@ -1,4 +1,3 @@
-from multiprocessing import context
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (ListView , DetailView ,CreateView , UpdateView , DeleteView)
