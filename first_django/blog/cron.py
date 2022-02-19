109c3b740b48a7db4e06aebee2f232a6116cad18
saikrishnavadali05 <saikrishnavadali05@gmail.com>
2022-02-19 11:21:12 +0530

    to check:server stopped feb19 11:21

--- first_django/blog/cron.py
+++ first_django/blog/cron.py
@@ -11,18 +11,18 @@ today=now.strftime("%Y-%m-%d")
 
 to = 'saikrishnavadali05@gmail.com'
 to2 = 'raviteja.sssihl@gmail.com'
-GMAIL_ID = 'pseudotesting5@gmail.com'
-GMAIL_PSWD = 'omsrisairam'
+GMAIL_ID = 'multiplewishes.py@gmail.com'
+GMAIL_PSWD = 'swamigrace'
 
 
 def sending_mail_job():   
-    username = CustomerHBD.objects.filter(date = today)
-    date_today_db=str(username)
-    print(date_today_db[25:35])
-    if str(date_today_db) == str(today): 
+    #username = CustomerHBD.objects.filter(date = today)
+    #date_today_db=str(username)
+    #print(date_today_db[25:35])
+    #if str(date_today_db) == str(today): 
         s = smtplib.SMTP('smtp.gmail.com',587)
         s.starttls()
         s.login(GMAIL_ID, GMAIL_PSWD)
-        s.sendmail(GMAIL_ID, to, f"I could not deliver on thy Fathers B`Day,But product is ready on Thy parents Marriage Day! feeling satisfied")
-        s.sendmail(GMAIL_ID, to2, f"Happy Birthday bro you are gr8 feeling proud")
+        s.sendmail(GMAIL_ID, to, f"First testing wish")
+        s.sendmail(GMAIL_ID, to2, f"first wish")
         s.quit()
